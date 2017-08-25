ed5a307e33cf2d305b7a608982f3aac6d8bba932
Kyleen Lopez <kayelle.03@gmail.com>
2017-08-25 12:00:09 +1000

    Merged all three DVRs Removed Void Trials and problematic arenas.

---
+++ DeliScript.R
@@ -0,0 +1,45 @@
+DVR1=read.csv("DVR1Data.csv")
+DVR2=read.csv("DVR2Data.csv")
+DVR3=read.csv("DVR3Data.csv")
+
+
+install.packages("car")
+
+datTest <- airquality
+head(datTest)
+str(datTest)
+
+datTest$Month <- factor(datTest$Month)
+
+car::scatterplotMatrix(~log(Ozone) + Wind, diagonal = "histogram", data = datTest)
+
+plot(log(Ozone) ~ Wind, data = datTest, ylab = "Log Transformed Ozone", xlab = "Wind", pch = 16)
+text(labels = rownames(datTest), y = log(datTest$Ozone), x = datTest$Wind)
+
+length(unique(datTest$Month))
+
+install.packages("dplyr")
+library(dplyr)
+summarise(group_by(datTest, Month), mean = mean(Ozone, na.rm = TRUE))
+
+xtabs(~Month, data=datTest)
+
+
+
+#HONOURS
+Merged = merge(DVR1, DVR2, all=TRUE)
+DVR = merge(Merged, DVR3, all = TRUE)
+
+length(unique(DVR$LizID))
+str(Merged)
+head(DVR1)
+
+#PLOTS
+plot(Hide_Duration ~ TotalDist, data = DVR)
+
+
+#REMOVING VOID TRIALS
+summary(DVR$Arena_Duration)
+DVR[is.na(DVR$Arena_Duration),]
+is.na(DVR$Arena_Duration)
+DVR <- DVR[!is.na(DVR$Arena_Duration),]
